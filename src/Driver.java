4a7818a165b082462a052563fc43c1c31e214d73
chaldeos <chaldeos.y@gmail.com>
2014-01-23 00:04:57 +0200

    First version of driver

---
+++ src/Driver.java
@@ -0,0 +1,22 @@
+
+public class Driver{
+
+	int uid, sex, type;
+	String username, password, firstname, surname, tin, adresss, phone;
+	
+	
+	public Driver(int uid, String username, String password, String firstname, String surname,String tin, String adresss, String phone, int sex, int type){
+		this.uid = uid;
+		this.username = username;
+		this.password = password;
+		this.firstname = firstname;
+		this.surname = surname;
+		this.tin = tin;
+		this.adresss = adresss;
+		this.phone = phone;
+		this.sex = sex;
+		this.type = type;
+		
+	}
+	
+}
