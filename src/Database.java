15e31ee4b5a39e5959240fec8ef0f9bad35a9325
magkopian <m.agkopian@gmail.com>
2014-01-22 18:55:30 +0200

    Added InsertToDatabase and FetchFromDatabase signatures

--- src/Database.java
+++ src/Database.java
@@ -3,8 +3,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Database {
+	Connection conn = null;
+	
     public void MysqlConnect() {
-    	Connection conn = null;
 
     	try {
     		
@@ -22,5 +23,19 @@ public class Database {
     		System.out.println("SQLState: " + ex.getSQLState());
     		System.out.println("VendorError: " + ex.getErrorCode());
     	}
+    	
 	}
+    
+    public void InsertToDatabase(String table, String fields, String values) {
+    	// Construct Query
+    	
+    	//Execute
+    }
+    
+    public void FetchFromDatabase(String table, String fields, String condition) {
+    	// Construct Query
+    	
+    	// Execute
+    }
+    
 }
