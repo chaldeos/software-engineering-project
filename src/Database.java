ebfcb27a0db02aca2f892419bc5b53eaf926a29e
magkopian <m.agkopian@gmail.com>
2014-01-22 18:30:44 +0200

    Database Class and test Main Class Added

---
+++ src/Database.java
@@ -0,0 +1,26 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+    public void MysqlConnect() {
+    	Connection conn = null;
+
+    	try {
+    		
+    		try {
+				Class.forName("com.mysql.jdbc.Driver").newInstance();
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+    		
+    		// Connect to Database
+    		conn = DriverManager.getConnection("jdbc:mysql://localhost/shoft-eng-proj?" + "user=Manolis&password=qwe/789");
+
+    	} catch (SQLException ex) {
+    		System.out.println("SQLException: " + ex.getMessage());
+    		System.out.println("SQLState: " + ex.getSQLState());
+    		System.out.println("VendorError: " + ex.getErrorCode());
+    	}
+	}
+}
