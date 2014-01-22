83b798b611c41d57487ad89385b95ff91484df5d
magkopian <m.agkopian@gmail.com>
2014-01-23 00:23:28 +0200

    Fixed small mistake

--- src/Driver.java
+++ src/Driver.java
@@ -2,17 +2,17 @@
 public class Driver{
 
 	int uid, sex, type;
-	String username, password, firstname, surname, tin, adresss, phone;
+	String username, password, firstname, surname, tin, adress, phone;
 	
 	
-	public Driver(int uid, String username, String password, String firstname, String surname,String tin, String adresss, String phone, int sex, int type){
+	public Driver(int uid, String username, String password, String firstname, String surname, String tin, String adress, String phone, int sex, int type){
 		this.uid = uid;
 		this.username = username;
 		this.password = password;
 		this.firstname = firstname;
 		this.surname = surname;
 		this.tin = tin;
-		this.adresss = adresss;
+		this.adress = adress;
 		this.phone = phone;
 		this.sex = sex;
 		this.type = type;
