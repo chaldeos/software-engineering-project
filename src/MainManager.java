e0cea10135b93f45758ee53b9bb065505e7d2732
chaldeos <chaldeos.y@gmail.com>
2014-01-24 22:12:55 +0200

    Main of manager

---
+++ src/MainManager.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.io.*;
+//import java.text.DateFormat;
+
+public class MainManager extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args) {
+		
+		LoginFrameManager window = new LoginFrameManager();
+		window.setTitle("Είσοδος χρήστη");
+		window.setVisible(true);
+		
+	}
+
+}
