aaf28e3ebd9552614b08e5281599626f8c9b1024
magkopian <m.agkopian@gmail.com>
2014-01-24 15:06:42 +0200

    Updated UnitTestValidation Class

--- src/UnitTestValidation.java
+++ src/UnitTestValidation.java
@@ -8,14 +8,16 @@ public class UnitTestValidation {
 	@Test
 	public void testValidateUsername() {
 		assertEquals("Result", "The username cannot be empty", Validation.validateUsername(""));
+		assertEquals("Result", "The username cannot be empty", Validation.validateUsername(null));
 		assertEquals("Result", "Username must be at least 4 characters", Validation.validateUsername("abc"));
-		assertEquals("Result", null, Validation.validateUsername("Νίκος"));
-		assertEquals("Result", null, Validation.validateUsername("abcd"));
+		assertEquals("Result", null, Validation.validateUsername("Νίκος")); // If Username "Νίκος" not already exists in users table
+		assertEquals("Result", null, Validation.validateUsername("abcd")); // If Username "abcd" not already exists in users table
 	}
 	
 	@Test
 	public void testValidatePassword() {
 		assertEquals("Result", "The password cannot be empty", Validation.validatePassword(""));
+		assertEquals("Result", "The password cannot be empty", Validation.validatePassword(null));
 		assertEquals("Result", "Password must be at least 6 characters", Validation.validatePassword("12345"));
 		assertEquals("Result", null, Validation.validatePassword("88888888"));
 		assertEquals("Result", null, Validation.validatePassword("password"));
@@ -24,6 +26,7 @@ public class UnitTestValidation {
 	@Test
 	public void testValidateFirstname() {
 		assertEquals("Result", "The firstname cannot be empty", Validation.validateFirstname(""));
+		assertEquals("Result", "The firstname cannot be empty", Validation.validateFirstname(null));
 		assertEquals("Result", null, Validation.validateFirstname("Νίκος"));
 		assertEquals("Result", null, Validation.validateFirstname("Αλ"));
 	}
@@ -31,6 +34,7 @@ public class UnitTestValidation {
 	@Test
 	public void testValidateSurname() {
 		assertEquals("Result", "The surname cannot be empty", Validation.validateSurname(""));
+		assertEquals("Result", "The surname cannot be empty", Validation.validateSurname(null));
 		assertEquals("Result", null, Validation.validateSurname("Νίκος"));
 		assertEquals("Result", null, Validation.validateSurname("Αλ"));
 	}
@@ -38,6 +42,7 @@ public class UnitTestValidation {
 	@Test
 	public void testValidateTin() {
 		assertEquals("Result", "The Tax Identify Number cannot be empty", Validation.validateTin(""));
+		assertEquals("Result", "The Tax Identify Number cannot be empty", Validation.validateTin(null));
 		assertEquals("Result", "Invalid Tax Identify Number", Validation.validateTin("12345"));
 		assertEquals("Result", "Invalid Tax Identify Number", Validation.validateTin("12345678"));
 		assertEquals("Result", "Invalid Tax Identify Number", Validation.validateTin("12345678a"));
@@ -49,6 +54,7 @@ public class UnitTestValidation {
 	@Test
 	public void testValidateAddress() {
 		assertEquals("Result", "The address cannot be empty", Validation.validateAddress(""));
+		assertEquals("Result", "The address cannot be empty", Validation.validateAddress(null));
 		assertEquals("Result", null, Validation.validateAddress("Πηλέως 55"));
 		assertEquals("Result", null, Validation.validateAddress("Πηλέως"));
 	}
@@ -56,6 +62,7 @@ public class UnitTestValidation {
 	@Test
 	public void testValidatePhone() {
 		assertEquals("Result", "The phone cannot be empty", Validation.validatePhone(""));
+		assertEquals("Result", "The phone cannot be empty", Validation.validatePhone(null));
 		assertEquals("Result", "Invalid phone number", Validation.validatePhone("12345"));
 		assertEquals("Result", "Invalid phone number", Validation.validatePhone("12345678"));
 		assertEquals("Result", "Invalid phone number", Validation.validatePhone("12345678a"));
@@ -79,6 +86,14 @@ public class UnitTestValidation {
 		assertEquals("Result", null, Validation.validateType(2));
 		assertEquals("Result", "Invalid user type value", Validation.validateType(4));
 		assertEquals("Result", "Invalid user type value", Validation.validateType(-1));
+	}
+	
+	@Test
+	public void testValidateReport() {
+		assertEquals("Result", "The report cannot be empty", Validation.validateReport(null));
+		assertEquals("Result", "The report cannot be empty", Validation.validateReport(""));
+		assertEquals("Result", null, Validation.validateReport("fhcgncfgncfg"));
+		assertEquals("Result", null, Validation.validateReport("a"));
 	}
 	
 	@Test
