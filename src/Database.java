82f23806e670cfbf8585887184d81a645657d118
Manolis Agkopian <m.agkopian@gmail.com>
2014-01-25 01:36:54 +0200

    Update Database.java

--- src/Database.java
+++ src/Database.java
@@ -21,7 +21,7 @@ public class Database {
     		}
     		
     		// Connect to Database
-    		conn = DriverManager.getConnection("jdbc:mysql://localhost/shoft-eng-proj?" + "user=Manolis&password=qwe/789");
+    		conn = DriverManager.getConnection("jdbc:mysql://localhost/shoft-eng-proj?" + "user=root&password=");
 
     	} catch (SQLException e) {
     		System.out.println("SQLException: " + e.getMessage());
