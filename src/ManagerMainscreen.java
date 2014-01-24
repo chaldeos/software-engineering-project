0fe5a213b21e9426f9a13dc3d49dc84eceb90de2
efthialex <efthialex.mil@gmail.com>
2014-01-24 15:54:12 +0200

    minor changes

--- src/ManagerMainscreen.java
+++ src/ManagerMainscreen.java
@@ -6,6 +6,7 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.JButton;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
@@ -18,7 +19,7 @@ public class ManagerMainscreen extends JFrame {
 	/**
 	 * Launch the application.
 	 */
-	/*public static void main(String[] args) {
+	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -30,21 +31,14 @@ public class ManagerMainscreen extends JFrame {
 			}
 		});
 	}
-	*/
+	
 	/**
 	 * Create the frame.
 	 */
 	
-	public void run() {
-		try {
-			ManagerMainscreen frame = new ManagerMainscreen();
-			frame.setVisible(true);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
 	
 	public ManagerMainscreen() {
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
@@ -61,21 +55,30 @@ public class ManagerMainscreen extends JFrame {
 		JButton btnBuses = new JButton("Οχήματα");
 		btnBuses.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				ManagerBusesWnd window = new ManagerBusesWnd(); 
+				window.setTitle("Οχήματα");
+				window.setVisible(true);
 			}
 		});
 		btnBuses.setBounds(70, 130, 117, 60);
 		contentPane.add(btnBuses);
 		
 		JButton btnEmployees = new JButton("Υπάλληλοι");
+		
 		btnEmployees.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+
 			}
 		});
+		
 		btnEmployees.setBounds(260, 130, 117, 60);
 		contentPane.add(btnEmployees);
 		
 		JButton btnLogOut = new JButton("Αποσύνδεση");
 		btnLogOut.setBounds(301, 17, 131, 25);
 		contentPane.add(btnLogOut);
+		
+		
+		
 	}
 }
